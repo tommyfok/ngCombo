aa08b1743d3733e2261cefea1a1a3b6c9818cf15
tommyfok <154274174@qq.com>
2016-01-04 15:32:29 +0800

    update

--- ngCombo.js
+++ ngCombo.js
@@ -1,13 +1,12 @@
 var ngComboTpl = ''
-+'<div class="ngCombo">'
-+'  <div class="ngComboMask" ng-hide="showList" ng-click="showList=true"></div>'
-+'  <div class="selectedItems">'
++'<div class="ngCombo" ng-mouseleave="showList=false">'
++'  <div class="ngComboMask" ng-hide="showList" ng-click="showListAndFocus()"></div>'
++'  <div class="selectedItems" ng-hide="showList">'
 +'    <div class="placeholder" ng-bind="placeholder" ng-hide="selectedItems.length"></div>'
 +'    <span ng-repeat="item in selectedItems" ng-bind="item.text || item" ng-click="remove(item)"></span>'
 +'  </div>'
-+'  <span class="dropdownIcon" ng-click="showList=!showList">{{showList ? \'－\' : \'+\'}}</span>'
-+'  <div ng-show="showList">'
-+'    <input class="comboQuery" ng-model="query" placeholder="在此输入搜索内容">'
++'  <div ng-show="showList" class="show-list">'
++'    <input class="comboQuery" ng-model="query" placeholder="在此输入搜索内容" ng-keyup="addOne($event)">'
 +'    <div class="optionList">'
 +'      <div class="option" ng-class="{selected: isSelected(item)}"'
 +'           ng-repeat="item in data | filter : query" ng-click="toggle(item)">'
@@ -28,10 +27,26 @@ angular.module('ngCombo', [])
       placeholder: '@placeholder'
     },
     require: 'ngModel',
+    controller: function ($scope, $filter) {
+      $scope.addOne = function (event) {
+        var shownItems = $filter('filter')($scope.data, $scope.query);
+        if (shownItems.length === 1 && event.which == 13) {
+          $scope.add(shownItems[0]);
+        }
+      };
+    },
     link: function (scope, elem, attrs, ngModelCtrl) {
       scope.selectedItems = [];
       scope.input = scope.input || [];
       scope.placeholder = scope.placeholder || '点击选择';
+
+      scope.showListAndFocus = function () {
+        scope.showList = true;
+        // make this async
+        setTimeout(function () {
+          elem[0].querySelector('.comboQuery').focus();
+        }, 0);
+      };
 
       scope.add = function (item) {
         if (scope.selectedItems.indexOf(item) === -1) {
